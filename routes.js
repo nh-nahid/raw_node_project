629627d14e21a8096972da58d3e6ce2c1480c5df
Nahid Hossain <nahid4510@gmail.com>
2026-06-08 00:13:22 +0600

    setup user guide

--- routes.js
+++ routes.js
@@ -1,9 +1,11 @@
 
 // dependencies
 const { sampleHandler } = require('./helpers/routeHandlers/sampleHandler');
+const { userHandler } = require('./helpers/routeHandlers/userHandler');
 
 const routes = {
     sample: sampleHandler, 
+    user: userHandler
 }
 
 module.exports = routes;

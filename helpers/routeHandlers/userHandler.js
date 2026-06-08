a3a32e8a0f6ac031a1f8e2b686be5b1d68cacb83
Nahid Hossain <nahid4510@gmail.com>
2026-06-08 13:13:15 +0600

    delete api implemented

--- helpers/routeHandlers/userHandler.js
+++ helpers/routeHandlers/userHandler.js
@@ -188,7 +188,40 @@ handler._users.put = (requestProperties, callback) => {
 };
 
 handler._users.delete = (requestProperties, callback) => {
-    
+    // check the phone number if valid
+  const phone =
+    typeof requestProperties.queryStringObject.phone === "string" &&
+    requestProperties.queryStringObject.phone.trim().length === 11
+      ? requestProperties.queryStringObject.phone
+      : false;
+
+      if(phone) {
+        // loopup the user
+
+        data.read('users', phone, (err1, userData) => {
+            if(!err1 && userData){
+                data.delete('users', phone, (err2) => {
+                    if(!err2){
+                        callback(200, {
+                            message: 'User has been successully deleted!'
+                        })
+                    } else{
+                        callback(500, {
+                            message: 'There was a server side error!'
+                        })
+                    }
+                })
+            } else{
+                callback(500, {
+                error: "There was a problem in the server side"
+        })
+            }
+        })
+      }else {
+        callback(400, {
+            error: "There was a problem in your request"
+        })
+      }
 };
 
 module.exports = handler;

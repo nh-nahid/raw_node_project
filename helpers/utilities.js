d484b7de330c3975a2b8ab1d2ddf006547067798
Nahid Hossain <nahid4510@gmail.com>
2026-06-08 19:56:15 +0600

    token route setup, token post, get and put api implemented

--- helpers/utilities.js
+++ helpers/utilities.js
@@ -29,5 +29,25 @@ utilities.hash = (str) => {
     }
 }
 
+// create random string
+utilities.createRandomString = (strlength) => {
+    let length = strlength;
+    length = typeof(strlength) === 'number' && strlength > 0 ? strlength: false;
+
+    if(length){
+       let possiblecharacters = 'abcdefghijklmnopqrstuvwxyz123456789';
+        let output = '';
+
+        for(let i=1; i<= length; i+=1){
+            let randomCharacter = possiblecharacters.charAt(Math.floor(Math.random() * possiblecharacters.length)
+        );
+        output += randomCharacter;
+        }
+        return output
+    }else {
+        return false;
+    }
+}
+
 
 module.exports = utilities;
--- routes.js
+++ routes.js
@@ -2,10 +2,12 @@
 // dependencies
 const { sampleHandler } = require('./helpers/routeHandlers/sampleHandler');
 const { userHandler } = require('./helpers/routeHandlers/userHandler');
+const { tokenHandler } = require('./helpers/routeHandlers/tokenHandler');
 
 const routes = {
     sample: sampleHandler, 
-    user: userHandler
+    user: userHandler,
+    token: tokenHandler,
 }
 
 module.exports = routes;

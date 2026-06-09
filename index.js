fc73fc23560660347fc0a61515b3e7f845f8f9ce
Nahid Hossain <nahid4510@gmail.com>
2026-06-09 17:34:34 +0600

    removed secret key

--- index.js
+++ index.js
@@ -5,14 +5,12 @@ const http = require('http');
 const { handleReqRes } = require('./helpers/handleReqRes');
 const environment = require('./helpers/environments');
 const data = require('./lib/data');
-const { sendTwilioSms } = require('./helpers/notifications')
 // app object - scaffolding
 const app = {};
 
 
 // app config
-// sendTwilioSms('01617121519', 'Hello world', (err) => {
-//     console.log(`This is the error`, err);
+
     
 // })
 // data.delete('test', 'newFile', (err) => {

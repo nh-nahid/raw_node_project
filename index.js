8c8bfc5c8eda8e566d87dde07e3c0c42159b21bc
Nahid Hossain <nahid4510@gmail.com>
2026-06-09 23:04:45 +0600

    url monitoring implemented

--- index.js
+++ index.js
@@ -1,31 +1,20 @@
 // Title: 
 
 // dependencies
-const http = require('http');
-const { handleReqRes } = require('./helpers/handleReqRes');
-const environment = require('./helpers/environments');
-const data = require('./lib/data');
+const server = require('./lib/server');
+const workers = require('./lib/worker');
+
 // app object - scaffolding
 const app = {};
 
+app.init = () => {
+    // start the server
+    server.init();
 
-// app config
-
-    
-// })
-// data.delete('test', 'newFile', (err) => {
-//     console.log( err);
-    
-// })
-// create server
-app.createServer = () => {
-    const server = http.createServer(app.handleReqRes);
-    server.listen(environment.port, () => {
-        console.log(`lestening to port ${environment.port}`);
-    })
+    // start the workers
+    workers.init();
 }
 
-// handle request response
-app.handleReqRes = handleReqRes;
+app.init();
 
-app.createServer()
+module.exports = app;
